921b7d49535098eed77bbf852d808e14efb508bc
OmKumar2003 <omkumar742003@gmail.com>
2024-07-29 18:29:08 +0530

    first commit

---
+++ my-dns/src/main/java/com/github/omkumar2003/App.java
@@ -0,0 +1,13 @@
+package com.github.omkumar2003;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
